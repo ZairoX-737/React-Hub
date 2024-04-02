0fa76d1ec6a28ccd67af98a9f7e75036c6d70ce9
ZairoX-737 <kirill5525@bk.ru>
2024-04-02 12:22:37 +0300

    ./Notes 1.2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { nanoid } from 'nanoid';
 import NotesList from "./components/NotesList";
+import Search from "./components/Search";
+import Header from "./components/Header";
 
  const App = () =>{
   const [notes, setNotes] = useState([
@@ -25,9 +27,58 @@ import NotesList from "./components/NotesList";
       date: "02/04/2024",
     },
   ]);
-  return <div className="container">
-    <NotesList notes={notes}/>
-  </div>
- }
+
+  const [searchText, setSearchText] = useState('');
+
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    const savedNotes = JSON.parse(
+        localStorage.getItem('react-notes-data')
+      );
+
+      if(savedNotes){
+        setNotes(savedNotes);
+      };
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(
+      'react-notes-data', 
+      JSON.stringify(notes)
+    );
+  }, [notes]);
+
+  const addNote = (text) => {
+    const date = new Date();
+    const newNote = {
+      id: nanoid(),
+      text: text,
+      date: date.toLocaleDateString('en-US'),
+    }
+    const newNotes = [...notes, newNote];
+    setNotes(newNotes);
+  }
+
+  const deleteNote = (id) => {
+    const newNotes = notes.filter((note)=> note.id !== id);
+    setNotes(newNotes)
+  }
+
+  return (
+    <div className={`${darkMode && 'dark-mode'}`}>
+      <div className="container">
+        <Header handleToggleDarkMode={setDarkMode} />
+        <Search handleSearchNote={setSearchText} />
+        <NotesList
+          notes={notes.filter((note) =>
+            note.text.toLowerCase().includes(searchText)
+          )}
+          handleAddNote={addNote}
+          handleDeleteNote={deleteNote}
+        />
+      </div>
+    </div>
+  );}
 
  export default App
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+   <React.StrictMode> {/*delete for localStorage work*/} 
     <App />
   </React.StrictMode>
 );
